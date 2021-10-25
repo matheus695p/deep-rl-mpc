21da7a4e43a215f23d7030194650de77c1284d4e
matheus695p <Matheus_Pinto@mckinsey.com>
2021-10-25 11:22:24 -0300

    add: final controlers

--- model_DDPG.py
+++ model_DDPG.py
@@ -3,10 +3,13 @@ import pickle
 import warnings
 import numpy as np
 import pandas as pd
-from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.bench import Monitor
-from stable_baselines import PPO2
+from stable_baselines.common.noise import (NormalActionNoise,
+                                           OrnsteinUhlenbeckActionNoise,
+                                           AdaptiveParamNoiseSpec)
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines import DDPG
 from src.environment import DataFrameEnv
 from src.bounds import get_bounds_limits
 from src.callbacks import SaveOnBestTrainingRewardCallback
@@ -15,7 +18,7 @@ warnings.filterwarnings("ignore")
 # env settings
 seed = 0
 
-model_name = "PPO2"
+model_name = "DDPG"
 env_name = "TabularOptimization-{model_name}"
 file_name = "%s_%s_%s" % ("{model_name}", env_name, str(seed))
 
@@ -54,21 +57,26 @@ env = DataFrameEnv(df, model, features, control_variables, target, bounds)
 # recommendations, context and control variables
 rec_control_variables = ["rec_" + i for i in control_variables]
 context_variables = env.context_variables
+
 # monitor the complete environment
 env = Monitor(env, log_dir)
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: env])
-
 # Create the callback: check every 1000 steps
 callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
 
 # actor critic agents
-agent = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./tensorboard/")
-agent.learn(total_timesteps=max_episodes, callback=callback)
+# the noise objects for DDPG
+n_actions = env.action_space.shape[-1]
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+agent = DDPG(MlpPolicy, env, verbose=2, action_noise=action_noise, tensorboard_log="./tensorboard/")
+agent.learn(total_timesteps=max_episodes)
 agent.save(f"agents/{model_name}")
 
 # reload agent
-agent = PPO2.load(f"agents/{model_name}")
+agent = DDPG.load(f"agents/{model_name}")
 
 # configuración del entorno
 test_env = DataFrameEnv(test_df, model, features, control_variables, target, bounds)
--- model_TD3.py
+++ model_TD3.py
@@ -1,21 +1,29 @@
 import os
+import gym
+import json
 import pickle
 import warnings
 import numpy as np
 import pandas as pd
-from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.bench import Monitor
-from stable_baselines import PPO2
+from stable_baselines.common.noise import (NormalActionNoise,
+                                           OrnsteinUhlenbeckActionNoise,
+                                           AdaptiveParamNoiseSpec)
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines import TD3
+
 from src.environment import DataFrameEnv
 from src.bounds import get_bounds_limits
 from src.callbacks import SaveOnBestTrainingRewardCallback
 warnings.filterwarnings("ignore")
 
+### NEED TO CHANGE MODEL NAME AND mode.load WITH CORRESPONDANT AGENT
+
 # env settings
 seed = 0
 
-model_name = "PPO2"
+model_name = "TD3"
 env_name = "TabularOptimization-{model_name}"
 file_name = "%s_%s_%s" % ("{model_name}", env_name, str(seed))
 
@@ -54,21 +62,26 @@ env = DataFrameEnv(df, model, features, control_variables, target, bounds)
 # recommendations, context and control variables
 rec_control_variables = ["rec_" + i for i in control_variables]
 context_variables = env.context_variables
+
 # monitor the complete environment
 env = Monitor(env, log_dir)
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: env])
-
 # Create the callback: check every 1000 steps
 callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
 
 # actor critic agents
-agent = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./tensorboard/")
-agent.learn(total_timesteps=max_episodes, callback=callback)
+# the noise objects for DDPG
+n_actions = env.action_space.shape[-1]
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+agent = TD3('MlpPolicy', env, action_noise=action_noise, verbose=1, tensorboard_log="./tensorboard/")
+agent.learn(total_timesteps=max_episodes)
 agent.save(f"agents/{model_name}")
 
 # reload agent
-agent = PPO2.load(f"agents/{model_name}")
+agent = TD3.load(f"agents/{model_name}")
 
 # configuración del entorno
 test_env = DataFrameEnv(test_df, model, features, control_variables, target, bounds)
